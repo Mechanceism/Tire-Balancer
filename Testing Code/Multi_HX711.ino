cb7dff1c4fc4522eda7e2f26f5c05ea8549f85cc
Mechanceism <designismsideas@gmail.com>
2025-03-12 02:31:46 -0400

    Update Multi_HX711.ino

--- Testing Code/Multi_HX711.ino
+++ Testing Code/Multi_HX711.ino
@@ -6,6 +6,9 @@ const int LOADCELL_SCK_PINS[] = {5, 7, 9};   // Clock pins
 
 HX711 scales[3];
 
+// Variables to store readings
+long Scale1 = 0, Scale2 = 0, Scale3 = 0;
+
 void setup() {
   Serial.begin(9600);
   
@@ -15,19 +18,27 @@ void setup() {
 }
 
 void loop() {
+  long readings[3] = {Scale1, Scale2, Scale3};  // Array for storing values
+
   for (int i = 0; i < 3; i++) {
     if (scales[i].is_ready()) {
-      long reading = scales[i].read();
-      Serial.print("HX711[");
-      Serial.print(i);
-      Serial.print("] reading: ");
-      Serial.println(reading);
+      readings[i] = scales[i].read();
     } else {
       Serial.print("HX711[");
       Serial.print(i);
       Serial.println("] not found.");
     }
   }
+
+  // Assign values to named variables
+  Scale1 = readings[0];
+  Scale2 = readings[1];
+  Scale3 = readings[2];
+
+  // Print values
+  Serial.print("Scale1: "); Serial.println(Scale1);
+  Serial.print("Scale2: "); Serial.println(Scale2);
+  Serial.print("Scale3: "); Serial.println(Scale3);
 
   delay(1000);
 }
